d6f10351cc44f3fa86ac39c3ca4e26297d08455b
Oguz Vatansever <ogzvatansever@gmail.com>
2023-02-23 23:07:09 +0300

    testing some ideas

--- api.py
+++ api.py
@@ -3,6 +3,15 @@ from flask_restful import Resource, Api
 from flask_cors import CORS
 import random
 import json
+import sqlite3
+
+con = sqlite3.connect('test.db')
+cursor = con.cursor()
+
+cursor.execute('CREATE TABLE IF NOT EXISTS Games (board TEXT)')
+
+con.commit()
+con.close()
 
 app = Flask(__name__)
 CORS(app)
@@ -34,6 +43,10 @@ cardranking = {"1":14,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"A":10,"B"
 carddeck = {"A1":"🂡","A2":"🂢","A3":"🂣","A4":"🂤","A5":"🂥","A6":"🂦","A7":"🂧","A8":"🂨","A9":"🂩","AA":"🂪","AB":"🂫","AD":"🂭","AE":"🂮","B1":"🂱","B2":"🂲","B3":"🂳","B4":"🂴","B5":"🂵","B6":"🂶","B7":"🂷","B8":"🂸","B9":"🂹","BA":"🂺","BB":"🂻","BD":"🂽","BE":"🂾","C1":"🃁","C2":"🃂","C3":"🃃","C4":"🃄","C5":"🃅","C6":"🃆","C7":"🃇","C8":"🃈","C9":"🃉","CA":"🃊","CB":"🃋","CD":"🃍","CE":"🃎","D1":"🃑","D2":"🃒","D3":"🃓","D4":"🃔","D5":"🃕","D6":"🃖","D7":"🃗","D8":"🃘","D9":"🃙","DA":"🃚","DB":"🃛","DD":"🃝","DE":"🃞"}
 kartlar = [Card('A1'),Card('A2'),Card('A3'),Card('A4'),Card('A5'),Card('A6'),Card('A7'),Card('A8'),Card('A9'),Card('AA'),Card('AB'),Card('AD'),Card('AE'),Card('B1'),Card('B2'),Card('B3'),Card('B4'),Card('B5'),Card('B6'),Card('B7'),Card('B8'),Card('B9'),Card('BA'),Card('BB'),Card('BD'),Card('BE'),Card('C1'),Card('C2'),Card('C3'),Card('C4'),Card('C5'),Card('C6'),Card('C7'),Card('C8'),Card('C9'),Card('CA'),Card('CB'),Card('CD'),Card('CE'),Card('D1'),Card('D2'),Card('D3'),Card('D4'),Card('D5'),Card('D6'),Card('D7'),Card('D8'),Card('D9'),Card('DA'),Card('DB'),Card('DD'),Card('DE')]
 
+# Buton yeni kartlar çeksin en son çekilen kartları ekrana göstersin.
+# Butona basıldığında api a fetch isteği atıcak o yeni deste çekicek
+# Ekran şuanlık sadece en son çekilen kartları gösteriyor 
+
 class PokerAPI(Resource) :
     def get(self) :
         cards = kartlar.copy()
@@ -44,7 +57,15 @@ class PokerAPI(Resource) :
             board.append(cards.pop())
         for i in range(2) :
             hand.append(cards.pop())
-        return json.dumps([obj.__dict__ for obj in board])
+        con = sqlite3.connect('test.db')
+        cursor = con.cursor()
+        #cursor.execute('INSERT INTO Games (BOARD) VALUES (?)',(json.dumps([obj.__dict__ for obj in board]),))
+        cursor.execute('SELECT BOARD FROM Games ORDER BY ID DESC LIMIT 1')
+        output = cursor.fetchone()
+        con.commit()
+        con.close()
+        #return json.dumps([obj.__dict__ for obj in board])
+        return output
 
 api.add_resource(PokerAPI, '/poker/')
 
